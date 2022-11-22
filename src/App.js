ca958b9e512ce10423759831ebce322818077d10
Vinay Ts <vinaydhoni937@gmail.com>
2022-11-22 10:37:40 +0530

    4thcommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './2.jpg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React-App
+          React-App7
         </a>
       </header>
     </div>
