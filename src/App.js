8d9a24e574f224dc4fb9183aa721eafe3480f0ef
Vinay Ts <vinaydhoni937@gmail.com>
2022-11-22 10:10:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi I AM L LAWLIET
+        </p>
+        <a
+          className="App-link"
+          href="https://vinay1stapp.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React-App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
